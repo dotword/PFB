8e8f01c16690244c727202fe2329cf154d3f14de
Carmen Salgueiro <carmen@dotword.es>
2024-06-15 09:36:48 +0200

    Login

--- back/server.js
+++ back/server.js
@@ -9,9 +9,13 @@ const app = express();
 const { PORT } = process.env;
 
 app.get('/', (req, res) => {
-  res.send('Hola desde PFB');
+    res.send('Hola desde PFB');
+});
+
+app.get('/login', (req, res) => {
+    res.status(201).send('Login!!!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server on ${PORT}`);
+    console.log(`Server on ${PORT}`);
 });
